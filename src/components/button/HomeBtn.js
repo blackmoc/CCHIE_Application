dbeebb0303bdc7c7228dfdae2621a3d225171dbc
Cymantha Blackmon <blackmoc@kean.edu>
2024-02-12 17:57:10 -0500

    File Update
    Remove old Files in order to add new ones

--- src/components/button/HomeBtn.js
+++
@@ -1,9 +0,0 @@
-import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
-export default function HomeBtn() {
-  return (
-    <Button component={Link} to={"/"}>
-      Return Home
-    </Button>
-  );
-}
